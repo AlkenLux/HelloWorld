75c1a741dc41ca8d7270e9dcab7be9d9217a54d6
Arthur <arthur.bargy@alken-lux.lu>
2015-08-14 12:10:29 +0200

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("HelloWorld");
+            Console.Out.WriteLine("HelloWorld");
+            Console.Out.WriteLine("HelloWorld");
+
+            var toto = new []{ "toto" , "tata", "titi" };
+            
+            foreach (var i in toto)
+            {
+                Console.Out.WriteLine("{0}", i);
+
+            }
+            Console.Read();
+        }
+    }
+}
